5bfc8c62c4e01b515a2398f9fd84af997c6b15c5
DARSHON CRUDUP <dlc756@yahoo.com>
2023-03-28 16:55:12 -0400

    removing nested repo

--- models/book.js
+++ models/book.js
@@ -7,9 +7,7 @@ const { Schema } = mongoose;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  status: { type: Boolean, required: true }, 
-  url: { type: String, required: true }
-  // status (like read or not read) google:  mongoDB schema variable types/options, have an array of options(x)
+  status: { type: Boolean, required: true }, // status (like read or not read) google:  mongoDB schema variable types/options, have an array of options
 
 });
 
